805d85219d7098faaaa88c62212b782b4e316ba2
andrey30353 <andrey30353@mail.ru>
2020-10-27 00:16:10 +0300

    Префаб решетки

--- Assets/Scripts/Level/Hatch.cs
+++ Assets/Scripts/Level/Hatch.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Hatch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite BrokenSprite;
+
+    /*
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        print(collision.gameObject.name);
+
+        var rb = collision.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            var mover = collision.GetComponent<Mover2D>();
+            if (mover != null)
+            {
+                mover.enabled = false;
+            }
+
+            var force = (AttractPoint.position - collision.transform.position).normalized * Force;
+            //print(direction);
+            //rb.velocity = direction * Force;
+            rb.AddForce(force);
+
+        }
+
+
+    }*/
+
+    public void Broke()
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
