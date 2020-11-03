c28499f2d3f93a9c4780c3e195fefc4a51d2e4eb
andrey30353 <andrey30353@mail.ru>
2020-11-03 13:01:29 +0300

    Удалил старые скрипты

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -3,20 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public class AgentProbably
-{
-    public AgentType Type;
-    public float RandomValue;
-}
-
 public class Settings : MonoBehaviour
 {
-    public Color LiveColor;
-
-    public Color HunterColor;
-    public Color InfectorColor;
-    public Color QueenColor;
     [Space]
     public Color InfectedColor;
 
